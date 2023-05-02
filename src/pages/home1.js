0f29061e83df0f3ee5819d1ed52fcaef30b478c4
Mikhael <116017125+mikhaelnice@users.noreply.github.com>
2023-05-02 15:45:08 +0100

    Create home1.js

---
+++ src/pages/home1.js
@@ -0,0 +1,16 @@
+import BottomBar from "../components/BottomBar";
+import CenterContent from "../components/CenterContent";
+import TopBar from "../components/TopBar";
+import "./home1.css";
+
+const Home1 = () => {
+  return (
+    <div>
+      <TopBar />
+      <CenterContent />
+      <BottomBar />
+    </div>
+  );
+};
+
+export default Home1;
